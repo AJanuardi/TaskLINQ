494b1a67a601a61d4369c2b5bad459a4f6875b06
Agung Januardi <agung.januardi@bsi.co.id>
2020-01-21 13:40:06 +0700

    solved problem LINQ

---
+++ JSON1/Program.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Linq;
+
+namespace JSONtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+             string json = @"[{
+                                ""id"": 323,
+                                ""username"": ""rinood30"",
+                                ""profile"": {
+                                ""full_name"": ""Shabrina Fauzan"",
+                                ""birthday"": ""1988-10-30"",
+                                ""phones"": [
+                                    ""08133473821"",
+                                    ""082539163912"",
+                                ],
+                                },
+                                ""articles"": [
+                                {
+                                    ""id"": 3,
+                                    ""title"": ""Tips Berbagi Makanan"",
+                                    ""published_at"": ""2019-01-03T16:00:00""
+                                },
+                                {
+                                    ""id"": 7,
+                                    ""title"": ""Cara Membakar Ikan"",
+                                    ""published_at"": ""2019-01-07T14:00:00""
+                                }
+                                ]
+                            },
+                            {
+                                ""id"": 201,
+                                ""username"": ""norisa"",
+                                ""profile"": {
+                                ""full_name"": ""Noor Annisa"",
+                                ""birthday"": ""1986-08-14"",
+                                ""phones"": [],
+                                },
+                                ""articles"": [
+                                {
+                                    ""id"": 82,
+                                    ""title"": ""Cara Membuat Kue Kering"",
+                                    ""published_at"": ""2019-10-08T11:00:00""
+                                },
+                                {
+                                    ""id"": 91,
+                                    ""title"": ""Cara Membuat Brownies"",
+                                    ""published_at"": ""2019-11-11T13:00:00""
+                                },
+                                {
+                                    ""id"": 31,
+                                    ""title"": ""Cara Membuat Brownies"",
+                                    ""published_at"": ""2019-11-11T13:00:00""
+                                }
+                                ]
+                            },
+                            {
+                                ""id"": 42,
+                                ""username"": ""karina"",
+                                ""profile"": {
+                                ""full_name"": ""Karina Triandini"",
+                                ""birthday"": ""1986-04-14"",
+                                ""phones"": [
+                                    ""06133929341""
+                                ],
+                                },
+                                ""articles"": []
+                            },
+                            {
+                                ""id"": 201,
+                                ""username"": ""icha"",
+                                ""profile"": {
+                                ""full_name"": ""Annisa Rachmawaty"",
+                                ""birthday"": ""1987-12-30"",
+                                ""phones"": [],
+                                },
+                                ""articles"": [
+                                {
+                                    ""id"": 39,
+                                    ""title"": ""Tips Berbelanja Bulan Tua"",
+                                    ""published_at"": ""2019-04-06T07:00:00""
+                                },
+                                {
+                                    ""id"": 43,
+                                    ""title"": ""Cara Memilih Permainan di Steam"",
+                                    ""published_at"": ""2019-06-11T05:00:00""
+                                },
+                                {
+                                    ""id"": 58,
+                                    ""title"": ""Cara Membuat Brownies"",
+                                    ""published_at"": ""2019-09-12T04:00:00""
+                                }
+                                ]
+                            }]";
+    
+           
+           var list = JsonConvert.DeserializeObject<List<User>>(json);
+
+           Console.WriteLine("======================================");
+           Console.WriteLine("User yang tidak memiliki nomor telepon");
+           var result1 = list.Where(i => (i.Profile.Phones).Length == 0);
+           foreach(var x in result1)
+           {   
+              Console.WriteLine(x.Username);
+           }
+
+           
+           Console.WriteLine("======================================");
+           Console.WriteLine("User yang memiliki Artikel");
+           var result2 = list.Where(i => (i.articles).Count > 1);
+           foreach(var x in result2)
+           {   
+                Console.WriteLine(x.Username);
+           }
+
+           
+           Console.WriteLine("======================================");
+           Console.WriteLine("User yang memiliki kata Annis pada nama");
+           var result3 = list.Where(i => (i.Profile.Fullname).Contains("Annis"));
+           foreach(var x in result3)
+           {
+               Console.WriteLine(x.Username);
+           }
+
+           
+           Console.WriteLine("======================================");
+           Console.WriteLine("User yang lahir pada tahun 1986");
+           var result4 = list.Where(i => (i.Profile.Birthday).Year == 1986);
+           foreach(var x in result4)
+           {
+               Console.WriteLine(x.Username);
+           }
+
+           
+           Console.WriteLine("======================================");
+           Console.WriteLine("User yang memiliki artikel pada tahun 2020");
+           try
+           {
+               var result5 = from l in list from x in l.articles where ((x.published_at).Year == 2020) select l.Profile.Fullname;
+               foreach(var i in result5)
+               {
+                   Console.WriteLine(i);
+               }
+           }
+           catch (Exception e)
+           {
+               Console.WriteLine("Artikel Tidak Ditemukan");
+               throw;
+           }
+
+           
+           Console.WriteLine("======================================");
+           Console.WriteLine("User yang memiliki judul tips");
+           try
+           {
+               var result6 = from l in list from x in l.articles where ((x.title).Contains("Tips")) select l.Profile.Fullname;
+               foreach(var i in result6)
+               {
+                   Console.WriteLine(i);
+               }
+           }
+           catch (Exception e)
+           {
+               Console.WriteLine("Artikel Tidak Ditemukan");
+               throw;
+           }
+
+            
+           Console.WriteLine("======================================");
+           Console.WriteLine("User yang publish artikel sebelum Agustus 2019");
+           var filterDate = new DateTime(2019,08,01);
+           try
+           {
+               var results = from l in list from x in l.articles where ((x.published_at) < filterDate) select l.Profile.Fullname;
+               var result7 = results.Distinct();
+               foreach(var i in result7)
+               {
+                   Console.WriteLine(i);
+               }
+           }
+           catch (Exception e)
+           {
+               Console.WriteLine("Artikel Tidak Ditemukan");
+               throw;
+           }
+           Console.WriteLine("======================================");    
+        }
+    }
+    public class User
+    {
+        [JsonProperty("id")]
+        public int Id {get;set;}
+        [JsonProperty("username")]
+        public string Username {get;set;}
+        public Profile Profile {get;set;}
+        public List<Articles> articles {get;set;} = null;
+
+    }
+
+    public class Profile
+    {
+        [JsonProperty("id")]
+        public int Id {get;set;}
+        [JsonProperty("full_name")]
+        public string Fullname {get;set;}
+        [JsonProperty("birthday")]
+        public DateTime Birthday {get;set;}
+        [JsonProperty("phones")]
+        public string[] Phones {get;set;} = {};
+
+    }
+
+    public class Articles
+    {
+        [JsonProperty("id")]
+        public int id {get;set;}
+        [JsonProperty("title")]
+        public string title {get;set;}
+        [JsonProperty("published_at")]
+        public DateTime published_at {get;set;}
+    }
+    
+}
---
+++ JSON3/Program.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+namespace JSON3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string json = @"[
+  {
+    ""inventory_id"": 9382,
+    ""name"": ""Brown Chair"",
+    ""type"": ""furniture"",
+    ""tags"": [
+      ""chair"",
+      ""furniture"",
+      ""brown""
+    ],
+    ""purchased_at"": 1579190471,
+    ""placement"": {
+      ""room_id"": 3,
+      ""name"": ""Sangkuriang""
+    }
+  },
+  {
+    ""inventory_id"": 9380,
+    ""name"": ""Big Desk"",
+    ""type"": ""furniture"",
+    ""tags"": [
+      ""desk"",
+      ""furniture"",
+      ""brown""
+    ],
+    ""purchased_at"": 1579190642,
+    ""placement"": {
+      ""room_id"": 3,
+      ""name"": ""Sangkuriang""
+    }
+  },
+  {
+    ""inventory_id"": 2932,
+    ""name"": ""LG Monitor 50 inch"",
+    ""type"": ""electronic"",
+    ""tags"": [
+      ""monitor""
+    ],
+    ""purchased_at"": 1579017842,
+    ""placement"": {
+      ""room_id"": 3,
+      ""name"": ""Sangkuriang""
+    }
+  },
+  {
+    ""inventory_id"": 232,
+    ""name"": ""Sharp Pendingin Ruangan 2PK"",
+    ""type"": ""electronic"",
+    ""tags"": [
+      ""ac""
+    ],
+    ""purchased_at"": 1578931442,
+    ""placement"": {
+      ""room_id"": 5,
+      ""name"": ""Dhanapala""
+    }
+  },
+  {
+    ""inventory_id"": 9382,
+    ""name"": ""Alat Makan"",
+    ""type"": ""tableware"",
+    ""tags"": [
+      ""spoon"",
+      ""fork"",
+      ""tableware""
+    ],
+    ""purchased_at"": 1578672242,
+    ""placement"": {
+      ""room_id"": 10,
+      ""name"": ""Rajawali""
+    }
+  }
+]";
+        User obj = new User();
+        var MyObj = JsonConvert.DeserializeObject<List<User>>(json);
+
+        Console.WriteLine("================================");
+        Console.WriteLine("Jumlah barang pada Sangkuriang: ");
+            int total = 0;
+            var result1 = MyObj.Where(i => (i.Placement.Name).Contains("Sangkuriang"));
+            foreach (var x in result1)
+            {
+                total++;
+                Console.WriteLine(x.Name);
+            }
+            Console.WriteLine("Total: " + total + " barang");
+
+        Console.WriteLine("================================");
+        Console.WriteLine("Jumlah elektronik: ");
+            int elektrik = 0;
+            var result2 = MyObj.Where(i => (i.Type).Contains("electronic"));
+            foreach (var x in result2)
+            {
+                elektrik++;
+                Console.WriteLine(x.Name);
+            }
+            Console.WriteLine("Total: " + elektrik + " barang");
+        
+        Console.WriteLine("================================");
+        Console.WriteLine("Jumlah furniture: ");
+            int furnitur = 0;
+            var result3 = MyObj.Where(i => (i.Type).Contains("furniture"));
+            foreach (var x in result3)
+            {
+                furnitur++;
+                Console.WriteLine(x.Name);
+            }
+            Console.WriteLine("Total: " + furnitur + " barang");
+        
+        Console.WriteLine("================================");
+        Console.WriteLine("Jumlah item yang dibayar pada 16 Jan 2020: ");
+            int bayar = 0;
+            DateTime hasil = new DateTime(2020, 01, 16);
+            string tgl1 = hasil.ToShortDateString();
+            string tgl = "";
+            foreach (var x in MyObj)
+            {
+                var date = DateTimeOffset.FromUnixTimeSeconds(x.PurchasedAt).DateTime.ToShortDateString();
+                tgl += date;
+                
+                if (tgl1 == tgl)
+                {
+                    var result4 = MyObj.Where(i => tgl == tgl1);
+                    bayar++;
+                    tgl="";
+                    Console.WriteLine("Pembelian 16 Januari 2020 :" + x.Name);
+                }
+            }
+            Console.WriteLine("Total: " + bayar + " barang");
+        
+        Console.WriteLine("================================");
+        Console.WriteLine("Jumlah barang cokelat: ");
+            int brown = 0;
+            var result5 = MyObj.Where(i=>(i.Tags).Contains("brown"));
+            foreach (var x in result5)
+            {
+                brown++;
+                Console.WriteLine(x.Name);
+            }
+            Console.WriteLine("Total: " + furnitur + " barang");
+            Console.WriteLine("================================");
+        
+        }
+        class User
+        {
+            [JsonProperty("inventory_id")]
+            public int InveroryId {get; set;}
+            [JsonProperty("name")]
+            public string Name {get;set;}
+            [JsonProperty("type")]
+            public string Type {get; set;}
+            [JsonProperty("purchased_at")]
+            public long PurchasedAt {get; set;}
+            [JsonProperty("placement")]
+            public Placement Placement {get; set;}
+            [JsonProperty("tags")]
+            public List<string> Tags {get; set;} = new List<string>();
+        }
+        class Placement
+        {
+            [JsonProperty("room_id")]
+            public int RoomId {get; set;}
+            [JsonProperty("name")]
+            public string Name {get; set;}
+        }
+    }
+}
